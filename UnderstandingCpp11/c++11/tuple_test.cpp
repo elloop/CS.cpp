27bd6fd6041182f5e5be0ab23209484a0e471463
elloop <elloop@foxmail.com>
2016-01-31 12:21:44 +0800

    t

--- UnderstandingCpp11/c++11/tuple_test.cpp
+++ UnderstandingCpp11/c++11/tuple_test.cpp
@@ -4,5 +4,13 @@ BEGIN_TEST(TupleTest, Basic, @);
 	std::tuple<int, int, int> x;
 	
 	std::string s("");
+
 	psln(s);
+END_TEST;
+
+BEGIN_TEST(TupleTest, Get, @);
+std::tuple<int, int, int> x;
+
+std::get<0>(x);
+
 END_TEST;
