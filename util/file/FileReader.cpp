b487dc58a32fd5768e743a26a76d3d9632b42ab3
elloop <elloop@foxmail.com>
2016-03-29 21:36:17 +0800

    format

--- util/file/FileReader.cpp
+++ util/file/FileReader.cpp
@@ -5,41 +5,48 @@ NS_BEGIN(elloop);
 
 FileReader* FileReader::instance_ = nullptr;
 
-FileReader* FileReader::getInstance() {
-    if (!instance_) {
+FileReader* FileReader::getInstance()
+{
+    if (!instance_)
+    {
         instance_ = new FileReader();
     }
     return instance_;
 }
 
-unsigned  char* FileReader::getFileData(const std::string& fileName, const char* mode, unsigned long * pSize) {
+unsigned  char* FileReader::getFileData(const std::string& fileName, const char* mode, unsigned long * pSize)
+{
     assert(!fileName.empty());
     unsigned char* res(nullptr);
     FILE * fp(nullptr);
-    do {
+    do
+    {
         fp = fopen(fileName.c_str(), mode);
         EL_BREAK_IF(!fp);
         fseek(fp, 0, SEEK_END);
         unsigned long fileSize(0);
-        if (!pSize) {
+        if (!pSize)
+        {
             fileSize = ftell(fp);
         }
-        else {
+        else
+        {
             fileSize = *pSize = ftell(fp);
         }
         fseek(fp, 0, SEEK_SET);
         res = new unsigned char[fileSize];
         fread(res, sizeof (unsigned char), fileSize, fp);
         fclose(fp);
-    } while (0);
+    }
+    while (0);
 
     return res;
 }
 
-void FileReader::purege() {
+void FileReader::purege()
+{
     EL_SAFE_DELETE(instance_);
 }
 
 NS_END(elloop);
 
-    
