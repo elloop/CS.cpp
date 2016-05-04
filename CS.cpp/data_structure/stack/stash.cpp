8a8653d4949ce7621a5112e1d6892c7279fd4831
elloop <elloop@foxmail.com>
2016-05-04 18:59:22 +0800

    prepare to leave the company, i feel upset somewhat

--- CS.cpp/data_structure/stack/stash.cpp
+++ CS.cpp/data_structure/stack/stash.cpp
@@ -1,44 +1,44 @@
 #include "stash.h"
 #include "inc.h"
 
-NS_BEGIN( elloop )
+NS_BEGIN(elloop)
 
 
-void Stash::push( void* data ) noexcept {
-  head_ = new Link( data, head_ );
+void Stash::push(void* data) noexcept {
+    head_ = new Link(data, head_);
 }
 
 void* Stash::peek() {
-  if ( nullptr == head_ ) {
-    return nullptr;
-  }
-  return head_->data_;
+    if (nullptr == head_) {
+        return nullptr;
+    }
+    return head_->data_;
 }
 
 void* Stash::pop() {
 
-  if ( head_ == nullptr) {
-    return nullptr;
-  }
-  Link* oldHead = head_;
-  head_ = oldHead->link_;
-  void* data = oldHead->data_;
-  delete oldHead;
-  oldHead = nullptr;
-  return data;
+    if (head_ == nullptr) {
+        return nullptr;
+    }
+    Link* oldHead = head_;
+    head_ = oldHead->link_;
+    void* data = oldHead->data_;
+    delete oldHead;
+    oldHead = nullptr;
+    return data;
 }
 
 bool Stash::empty() {
-  return ( head_ == nullptr );
+    return (head_ == nullptr);
 }
 
 void Stash::cleanup() {
-  auto temp = head_;
-  while ( temp != nullptr ) {
-    head_ = temp->link_;
-    delete temp;
-    temp = head_;
-  }
+    auto temp = head_;
+    while (temp != nullptr) {
+        head_ = temp->link_;
+        delete temp;
+        temp = head_;
+    }
 }
 
-NS_END( elloop )
+NS_END(elloop)
