c2353fc6335327229b4fbad0b48c9b335205a331
elloop <elloop@foxmail.com>
2016-05-28 23:17:09 +0800

    test va_arg with passing arg of type: pointer to basic type. and modify the input arg by pointer.

--- TrainingGround/src/grammar/va_args_test.cpp
+++ TrainingGround/src/grammar/va_args_test.cpp
@@ -5,6 +5,31 @@
 
 NS_BEGIN(elloop);
 
+void passStr(int count, ...) {
+    va_list varg;
+    va_start(varg, count);
+
+    for (int i=0; i<count; ++i) {
+        char* str = va_arg(varg, char*);
+        psln(str);
+        psln(strlen(str));
+    }
+
+    va_end(varg);
+}
+
+void passPtrInVargs(int count, ...) {
+    va_list varg;
+
+    va_start(varg, count);
+
+    for (int i=0; i<count; ++i) {
+        *va_arg(varg, int*) = 100;
+    }
+
+    va_end(varg);
+}
+
 int vsum(int count, ...) {
     va_list ap;
     va_start(ap, count);
@@ -19,7 +44,7 @@ int vsum(int count, ...) {
 }
 
 // ------------------ begin of new test case ------------------
-BEGIN_TEST(GrammarTest, VA_Args, @@);
+RUN_GTEST(GrammarTest, VA_Args, @@);
 
 // ------------------ function vsum using va_list ------------------
 EXPECT_EQ(0, vsum(0));
@@ -40,10 +65,12 @@ EXPECT_EQ(6, vsum1(1, 2.5, 3));
 EXPECT_EQ(6.5, vsum1(1.5, 2, 3));
 
 std::string t1 = typeid(decltype(vsum1(1, 2.5, 3))).name();
-EXPECT_STREQ("int", t1.c_str());
+// EXPECT_STREQ("int", t1.c_str());     // use this with visual c++ compiler.
+EXPECT_STREQ("i", t1.c_str());          // use this with clang++.
 
 std::string t2 = typeid( decltype( vsum1(1.5, 2, 3) ) ).name();
-EXPECT_STREQ("double", t2.c_str());
+// EXPECT_STREQ("double", t2.c_str());  // use this with visual c++ compiler.
+EXPECT_STREQ("d", t2.c_str());          // use this with clang++.
 
 // ------------------ template class vsum2 ------------------
 long s = vsum2<>::value;                         EXPECT_EQ(0, s);
@@ -60,6 +87,17 @@ s = vsum2<1, 1, 1, 1, 1, 1, 1, 1, 1, 1>::value;  EXPECT_EQ(10, s);
 long f = vsum2<>()();                            EXPECT_EQ(0, f);
 
 f = vsum2<1>()();                                EXPECT_EQ(1, f);
+
+
+// ========================= test passing ptr with vargs. ==========================
+int x(0);
+int y(0);
+passPtrInVargs(2, &x, &y);
+EXPECT_EQ(100, x);
+EXPECT_EQ(100, y);
+
+// ========================= test passing string with vargs. ==========================
+passStr(3, "hel", "hello\0hello", "");
 
 END_TEST;
 
