847d39dbba771b11e8041f022542d343645649ed
elloop <elloop@foxmail.com>
2016-09-15 09:28:34 +0800

    executable output path modification : from root/build/bin to {project_dir}/build/bin

--- Algorithm/src/data_structure/stack/matrix_explore.cpp
+++ Algorithm/src/data_structure/stack/matrix_explore.cpp
@@ -1,23 +1,50 @@
 #include "inc.h"
+#include "stack.h"
+#include <array>
 
 NS_BEGIN(elloop);
 
+using std::array;
 
 void matrixExplore() {
+    array<array<int, 5>, 5> mat = {
+        1, 0, 0, 1, 0,
+        1, 1, 1, 1, 0,
+        1, 0, 1, 0, 1,
+        1, 0, 1, 1, 0,
+        1, 1, 0, 1, 1,
+    };
+
+    struct Pos { 
+        size_t col; 
+        size_t row; 
+        bool operator == (const Pos& rhs) const {
+            return (rhs.row == row && rhs.col == col);
+        }
+    };
+
+    Pos outlet = {mat.size(), mat[0].size()};
+
+    Stack<Pos> st;
+    st.push({0, 0});
+
+    auto pos = st.top();
+    if (pos == outlet) {
+        pln("solved");
+    }
+    else {
+        pln("not the end");
+    }
 
 }
 
-RUN_GTEST(MatrixExplore, explore, @@);
+RUN_GTEST(StackTest, explore, @@);
 
-int matrix[][] = {
-    {1, 0, 0, 0, 0},
-    {1, 1, 1, 0, 0},
-    {0, 0, 1, 0, 0},
-    {1, 0, 0, 0, 0},
-    {1, 0, 0, 0, 0},
-};
+matrixExplore();
+
 
 END_TEST;
 
 
 NS_END(elloop);
+
